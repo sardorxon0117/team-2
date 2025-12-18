34fe9e15f734e138803d801b953024e0f175525d
Diyorbek <d.primqulov@student.pdp.university>
2025-12-18 08:29:15 +0500

    Add customer and payments routes to server
    Imported and registered customerRoutes and paymentsRoutes in server.js to handle '/customers' and '/payments' endpoints.

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ app.use(express.json());
 const pool = require("./db/db.js")
 
 app.use(express.json());
+// Routes
+  
+const customerRoutes = require("./routes/customerRoutes.js");
+const paymentsRoutes = require("./routes/paymentsRoutes.js");
+
+// Sardorxon
+
+app.use("/customers",customerRoutes);
+app.use("/payments",paymentsRoutes);
+
 
 app.listen(3000, () => {
   console.log("Server 3000 portda ishlamoqda");
